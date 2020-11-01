c993d8a3eeadac6380c3df5f569a5214a5e26d2d
xie3245 <fruitmxy@gmail.com>
2020-11-01 21:36:20 +0100

    more ignore

--- scripts/build_with_docker.sh
+++ scripts/build_with_docker.sh
@@ -4,4 +4,4 @@ BASEDIR=$(dirname "$0")
 cd $BASEDIR/../
 echo $(pwd)
 
-docker run -v $PWD:/workdir -w /workdir --device /dev/ttyACM0 avr-build-env:1.0 sh ./scripts/cmake_build.sh
+docker run --user 1000 -v $PWD:/workdir -w /workdir --device /dev/ttyACM0 avr-build-env:1.0 sh ./scripts/cmake_build.sh
