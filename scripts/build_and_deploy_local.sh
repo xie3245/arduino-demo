2dd3bd826dad92f653fdfda560de28815ebf138f
xie3245 <fruitmxy@gmail.com>
2020-11-02 18:42:16 +0100

    adjust local build script for multiple sources

--- scripts/build_and_deploy_local.sh
+++ scripts/build_and_deploy_local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "building file ${1}"
-avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o demo.o ${1}
-avr-gcc -o demo.bin demo.o
+echo "building files" "$@"
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -o demo.bin "$@"
 avr-objcopy -O ihex -R .eeprom demo.bin demo.hex 
 avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:demo.hex
+rm demo.bin demo.hex
